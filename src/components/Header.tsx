6b828911305493c4698edbf205e1d0bef4ee97f0
INSTAGATOR <naiseips@pm.me>
2023-11-07 12:41:40 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,8 +13,8 @@ export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
         </div>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <div className={styles.externalLinks}>
-            <a href="https://github.com/gamba-labs/gamba" target="_blank" rel="noreferrer">
-              Github
+            <a href="http://twofoldi.com" target="_blank" rel="noreferrer">
+              TwoFoldi
             </a>
             <a href="https://explorer.gamba.so" target="_blank" rel="noreferrer">
               Stats
